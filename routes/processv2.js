3b66599afff6eba98d83ac1ea50b85d60516ad5e
EC2 Default User <ec2-user@ip-172-31-21-172.ap-southeast-1.compute.internal>
2022-10-10 08:04:22 +0000

    upload finish

--- routes/processv2.js
+++ routes/processv2.js
@@ -6,30 +6,106 @@ var axios = require('axios');
 var { promisify } = require('util');
 const exec = promisify(require('child_process').exec);
 var db = require('../db');
+var crypto = require('crypto');
 router.use(express.json());
 
+const generateChecksum = (data, algo, enc) => {
+    return crypto.createHash(algo || 'md5').update(data, 'utf8').digest(enc || 'hex');
+}
+
 const uploadPath = path.join(process.cwd(), 'process');
-let state = ''
+
+var check_tracking = (filename, route, batch) => {
+    return new Promise((resolve, reject) => {
+        axios.get(`http://18.136.217.164:3001/process/tracking_status?filename=${filename}&route=${route}&batch=${batch}`).then((res) => {
+            resolve(res.data)
+        }).catch((error) => {
+            reject(error)
+        })
+    })
+}
+
+var tracking_start = (filename, route, batch) => {
+    return new Promise((resolve, reject) => {
+        axios.post('http://18.136.217.164:3001/process/tracking', {
+            stage: 'initial',
+            status: "Uploading",
+            fileName: filename,
+            route: route,
+            batch: batch
+        }).then((res) => {
+            resolve(res.data.tracking);
+        }).catch((error) => {
+            reject(error);
+        });
+    })
+}
+
+var tracking_update = (filename, route, batch, tracking, status) => {
+    axios.post('http://18.136.217.164:3001/process/tracking', {
+        stage: 'update',
+        status: status,
+        fileName: filename,
+        route: route,
+        batch: batch,
+        tracking: tracking
+    }).catch((e) => {
+        console.log(e);
+    });
+}
 
 router.get('/check/:filename', (req, res, next) => {
-    state = 'checking'
-    const filename = req.params.filename;
+    let filename = req.params.filename;
     let bytes = 0;
     let filePath = path.join(uploadPath, filename);
     if (fs.existsSync(filePath)) {
         try {
-            const fd = fs.openSync(filePath, "r");
-            const fileStat = fs.fstatSync(fd);
+            const fileStat = fs.statSync(filePath);
             bytes = fileStat.size;
         } catch (error) {
             console.log(error);
         }
+    } else {
+
     }
     res.send({bytes: bytes});
 });
 
+router.get('/upload_status', (req, res, next) => {
+    const { filename, route, batch } = req.query;
+    let filePath = path.join(uploadPath, filename);
+    let response = {
+        tracking: undefined,
+        bytes: 0
+    }
+    if (fs.existsSync(filePath)) {
+        check_tracking(filename, route, batch).then((data) => {
+            if (data.status) {
+                response.tracking = data.tracking;
+                try {
+                    const fileStat = fs.statSync(filePath);
+                    response.bytes = fileStat.size;
+                } catch (error) {
+                    console.log(error);
+                }
+            } else {
+                tracking_start(filename, route, batch).then((tracking) => {
+                    response.tracking = tracking;
+                })
+            }
+            res.send(response);
+            return;
+        })
+    } else {
+        tracking_start(filename, route, batch).then((tracking) => {
+            response.tracking = tracking;
+            res.send(response);
+        })
+        return;
+    }
+})
+
 router.get('/finish', function(req, res, next) {
-    state = 'finishing'
     fs.emptyDir(uploadPath, (err) => {
         if (err) {
             res.status(404).send('error');
@@ -40,7 +116,6 @@ router.get('/finish', function(req, res, next) {
 });
 
 router.post('/delete', (req, res, next) => {
-    state = 'deleting'
     body = req.body;
 
     for (const key of Object.keys(body)) {
@@ -49,8 +124,41 @@ router.post('/delete', (req, res, next) => {
     res.send('success');
 })
 
+router.post('/convert', (req, res, next) => {
+    body = req.body;
+
+    req.socket.setKeepAlive(true, 60000);
+
+    files = body.files;
+
+    req.on('timeout', () => {
+        console.log('request timed out');
+    });
+
+    let response = {}
+
+    let conversion = async () => {
+
+        for (const key of Object.keys(files)) {
+            let filename = `/home/ec2-user/team1_backend/process/${files[key].name}`;
+
+            let run;
+            if (fs.existsSync(filename)) {
+                run = await exec(`ffmpeg -y -f h264 -i ${filename} -threads 4 -c:v libx264 -preset ultrafast ${filename.split(".")[0]}.mp4`);
+                response[files[key].name] = 'ok';
+            } else {
+                response[files[key].name] = 'failed';
+            }
+            console.log(run)
+        }
+    }
+
+    conversion().then(() => {
+        res.send(response)
+    })
+});
+
 router.post('/process', (req, res, next) => {
-    state = 'processing'
     body = req.body;
 
     req.socket.setKeepAlive(true, 60000);
@@ -61,99 +169,216 @@ router.post('/process', (req, res, next) => {
 
     let video_types = ['.avi', '.mp4', '.mov'];
     let response = {}
+
+    let conversion = async (filename) => {
+        let run;
+        let output = `${filename.split(".")[0]}.mp4`
+        if (fs.existsSync(filename)) {
+            run = await exec(`ffmpeg -y -f h264 -i ${filename} -threads 2 -c:v libx264 -preset ultrafast ${output}`);
+            return output;
+        } else {
+            return filename;
+        }
+    }
+
     let execution = async () => {
-        console.log('Processing');
+        console.log('Processing', body);
         for (const key of Object.keys(body)) {
             let file = `/home/ec2-user/team1_backend/process/${body[key].filename}`;
+            let filenameArr = file.split(".");
+            if (filenameArr[filenameArr.length - 1] == 'ifv') {
+                console.log('converting');
+                tracking_update(body[key].filename, body[key].route, body[key].batch, body[key].tracking, 'Converting')
+                file = await conversion(file);
+                tracking_update(body[key].filename, body[key].route, body[key].batch, body[key].tracking, 'Converted')
+                console.log('conversion done');
+            }
             // console.log(file, fs.existsSync(file), ((video_types.includes(path.extname(`file`).toLowerCase())) && (!fs.existsSync(file))));
-            let gpx = `${file.split('.')[0]}.gpx`;
-            let csv = `${file.split('.')[0]}.csv`;
-            let xls = `${file.split('.')[0]}.xls`;
+            let gpx = `${filenameArr[0]}.gpx`;
+            let csv = `${filenameArr[0]}.csv`;
+            let xls = `${filenameArr[0]}.xls`;
+
+            // assume file process is ok
+            response[body[key].filename] = 'ok';
             if (!video_types.includes(path.extname(file).toLowerCase())) {
-                response[body[key].filename] = 'ok';
                 continue;
             }
             let run;
             if (fs.existsSync(gpx)) {
-                run = await exec(`conda run python /home/ec2-user/processing/process.py -F ${file} -G ${gpx} -R '${body[key].route}' -B ${body[key].batch}`);
+                run = await exec(`conda run python /home/ec2-user/processing/process.py -F ${file} -G ${gpx} -R '${body[key].route}' -B ${body[key].batch} -T ${body[key].tracking}`);
             } else if (fs.existsSync(csv) || fs.existsSync(xls)) {
-                run = await exec(`conda run python /home/ec2-user/processing/process.py -F  ${file} -SH ${fs.existsSync(csv) ? csv : xls} -R '${body[key].route}' -B ${body[key].batch}`);
+                run = await exec(`conda run python /home/ec2-user/processing/process.py -F  ${file} -SH ${fs.existsSync(csv) ? csv : xls} -R '${body[key].route}' -B ${body[key].batch} -T ${body[key].tracking}`);
             } else {
-                run = await exec(`conda run python /home/ec2-user/processing/process.py -F ${file} -R '${body[key].route}' -B ${body[key].batch}`);
+                run = await exec(`conda run python /home/ec2-user/processing/process.py -F ${file} -R '${body[key].route}' -B ${body[key].batch} -T ${body[key].tracking}`);
             }
             if (run.stderr.includes("ERROR ENCOUNTERED")) {
                 response[body[key].filename] = 'failed';
-            } else {
-                response[body[key].filename] = 'ok';
             }
             console.log(run) // for logging
         }
     }
     execution().then(() => {
-        console.log('ended', req.socket.destroyed);
         res.send(response);
     })
 });
 
-router.post('/', function(req, res) {
-    let busboy = req.busboy;
-    let body = req.body;
-    state = 'uploading'
-    let xFileName = req.headers['x-file-name'];
-    let xStartByte = parseInt(req.headers['x-start-byte'], 10);
-    let xFileSize = parseInt(req.headers['x-file-size'], 10);
 
-    console.log('starting upload session with headers:', xFileName, xStartByte, xFileSize);
+router.post('/chunk', function(req, res) {
+    let uploadState = 'uploading';
 
-    req.socket.on('close', () => {
-        console.log('session ended for', xFileName, xStartByte, xFileSize, state);
-        if (state == 'uploaded') {
-            axios.post('http://18.136.217.164:3001/process/tracking', {
-                stage: 'update',
-                status: 'Uploaded',
-                fileName: xFileName
-            });
-        }
-    });
+    const busboy = req.busboy;
 
-    if (xFileSize <= xStartByte) {
-        state = 'uploaded'
-        return res.send("File already uploaded");
+    const headers = req.headers;
+
+    const route = headers['x-route'];
+    const batch = headers['x-batch'];
+    const checksum = headers['x-file-checksum'];
+    const fileName = req.headers['x-file-name'];
+    const chunkStart = parseInt(req.headers['x-chunk-start'], 10);
+    const chunkSize = parseInt(req.headers['x-chunk-size'], 10);
+    const fileSize = parseInt(req.headers['x-file-size'], 10);
+    const tracking = parseInt(req.headers['x-tracking'], 10);
+
+    const filePath = path.join(uploadPath, fileName);
+
+    console.log('Starting upload session with headers: ', {
+        fileName: fileName,
+        route: route,
+        batch: batch,
+        checksum: checksum,
+        chunkStart: chunkStart,
+        chunkSize: chunkSize,
+        fileSize: fileSize,
+        tracking: tracking
+    })
+
+    if (fileSize == chunkStart) {
+        uploadState = 'uploaded';
+        res.send({status: 1});
     }
 
     req.pipe(busboy);
- 
+
     busboy.on("file", (fieldname, file, filename) => {
-        let filePath = path.join(uploadPath, filename);
         let fstream;
-        if (xStartByte) {
+        if (chunkStart) {
+            fs.truncateSync(filePath, chunkStart);
             fstream = fs.createWriteStream(filePath, {
-                flags: "a"
+                flags: "r+",
+                start: chunkStart
             });
         } else {
             fstream = fs.createWriteStream(filePath, {
                 flags: "w"
             });
         }
+
         file.pipe(fstream);
 
         file.on("error", (e) => {console.log(e)});
         file.on("limit", (e) => {console.log(e)});
 
         fstream.on("close", () => {
-            console.log("finished");
+            console.log(" - Chunk finished.");
         });
     });
 
     busboy.on("finish", function(a) {
-        state = 'uploaded'
-        console.log('session ended 2');
-        return res.send("ok");
+        state = 'uploaded';
+        let currFileSize = fs.statSync(filePath).size;
+
+        fs.open(filePath, 'r', function(errOpen, fd) {
+            if (errOpen) {
+                throw errOpen;
+            }
+            fs.read(fd, Buffer.alloc(chunkSize), 0, chunkSize, currFileSize - chunkSize, function (errRead, bytesRead, buffer) {
+                console.log('Validating checksum...');
+                let resChecksum = generateChecksum(buffer);
+                console.log('Upload session finished (busboy): ', {
+                    fileName: fileName,
+                    route: route,
+                    batch: batch,
+                    checksum: checksum,
+                    chunkStart: chunkStart,
+                    chunkSize: chunkSize,
+                    currentSize: fs.statSync(filePath).size,
+                    tracking: tracking,
+                    'File integrity?': `${resChecksum} == ${checksum} - ${resChecksum == checksum}`
+                });
+                if (resChecksum == checksum) {
+                    res.send({status: 1})
+                } else {
+                    res.send({status: 0})
+                }
+            })
+        })
     });
     busboy.on("error", function(a) {
-        return res.send("error");
+        return res.send({status: 0});
     });
+})
 
-});
+// router.post('/', function(req, res) {
+//     let busboy = req.busboy;
+//     let body = req.body;
+//     state = 'uploading'
+//     let xFileName = req.headers['x-file-name'];
+//     let xStartByte = parseInt(req.headers['x-start-byte'], 10);
+//     let xFileSize = parseInt(req.headers['x-file-size'], 10);
+//     let xTracking = parseInt(req.headers['x-tracking'], 10);
+
+//     let xFilePath = path.join(uploadPath, xFileName);
+
+
+
+//     console.log('starting upload session with headers:', xFileName, xStartByte, xFileSize);
+
+//     req.socket.on('close', () => {
+//         let fileStat = fs.statSync(filePath)
+//         let fileSize = fileStat.size;
+//         console.log('session ended for', xFileName, fileSize, xFileSize, xTracking, state);
+//         if (state == 'uploaded') {
+//             tracking_update(xFileName, xTracking, 'Uploaded')
+//         }
+//     });
+
+//     if (xFileSize <= xStartByte) {
+//         state = 'uploaded'
+//         return res.send("File already uploaded");
+//     }
+
+//     req.pipe(busboy);
+ 
+//     busboy.on("file", (fieldname, file, filename) => {
+//         let filePath = path.join(uploadPath, filename);
+//         let fstream;
+//         if (xStartByte) {
+//             fstream = fs.createWriteStream(filePath, {
+//                 flags: "a"
+//             });
+//         } else {
+//             fstream = fs.createWriteStream(filePath, {
+//                 flags: "w"
+//             });
+//         }
+//         file.pipe(fstream);
+
+//         file.on("error", (e) => {console.log(e)});
+//         file.on("limit", (e) => {console.log(e)});
+
+//         fstream.on("close", () => {
+//             console.log("finished");
+//         });
+//     });
+
+//     busboy.on("finish", function(a) {
+//         state = 'uploaded'
+//         console.log('session ended 2');
+//         return res.send("ok");
+//     });
+//     busboy.on("error", function(a) {
+//         return res.send("error");
+//     });
+// });
 
 module.exports = router;
